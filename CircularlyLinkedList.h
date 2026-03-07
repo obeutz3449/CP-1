bb2e4c3c7ccb35b464e9e8dc7d3940c8add585a6
obeutz3449 <obeutz3449@sdsu.edu>
2026-03-07 11:50:32 -0800

    Added usage of tail pointers Added insertMultiple method Minor cleanup

--- CircularlyLinkedList.h
+++ CircularlyLinkedList.h
@@ -5,6 +5,7 @@
 #ifndef CP_1_LINKEDLIST_H
 #define CP_1_LINKEDLIST_H
 #include <iostream>
+#include <vector>
 
 template<typename T> class Node {
     T data;
@@ -38,29 +39,29 @@ template<typename T> class CircularlyLinkedList {
             if (length == 0) {
                 head = new Node<T>(data);
                 head->setNext(head);
-                length++;
+                tail = head;
+                length = 1;
                 return true;
             }
-            if (index == 0) {
-                Node<T> *last = head;
-                while (last->getNext() != head) {
-                    last = last->getNext();
-                }
+            if (index%length == 0) {
                 Node<T> *temp = head;
                 head = new Node<T>(data);
                 head->setNext(temp);
-                last->setNext(head);
+                tail->setNext(head);
                 length++;
                 return true;
             }
             Node<T> *current = head;
-            for (int i = 0; i < index - 1; i++) {
+            for (int i = 0; i < (index - 1%length); i++) {
                 current = current->getNext();
             }
             auto *ins = new Node<T>(data);
             ins->setNext(current->getNext());
             current->setNext(ins);
             length++;
+            if (index%length == length-1) {
+                tail = ins;
+            }
             return true;
         }
 
@@ -73,25 +74,67 @@ template<typename T> class CircularlyLinkedList {
                     length = 0;
                     return true;
                 }
-                Node<T> *last = head;
-                while (last != head) {
-                    last = last->getNext();
-                }
-                last->setNext(head->getNext());
+                tail->setNext(head->getNext());
                 delete head;
-                head = last->getNext();
+                head = tail->getNext();
+                length--;
+                return true;
+            }
+            if (tail->getData() == data) {
+                auto *temp = tail;
+                tail = getNode(length - 2);
+                tail->setNext(head);
+                delete temp;
                 length--;
                 return true;
             }
             Node<T> *current = head;
             while (current->getNext()->getData() != data) {
                 current = current->getNext();
-                if (current->getNext() == head) return false;
+                if (current == head) return false;
             }
             Node<T> *temp = current->getNext();
             current->setNext(temp->getNext());
             delete temp;
             length--;
+            return true;
+        }
+
+        bool insertMultiple(std::pmr::vector<T> data, const int index) {
+            if (length + data.size() > 40) return false;
+            std::pmr::vector<Node<T>*> dataNode(data.size());
+            dataNode.push_back(new Node(data[0]));
+            for (int i = 1; i < data.size - 1; i++) {
+                auto* node = new Node<T>(data[i]);
+                dataNode[i - 1]->setNext(node);
+                dataNode.push_back(node);
+            }
+            Node<T> *dataHead = dataNode[0];
+            Node<T> *dataTail = dataNode[length - 1];
+            if (length == 0) {
+                head = dataHead;
+                tail = dataTail;
+                tail->setNext(head);
+                length+=data.size();
+                return true;
+            }
+            Node<T> *current = head;
+            if (index%length == 0) {
+                head = dataHead;
+                dataTail->setNext(current);
+                tail->setNext(dataHead);
+                length+=data.size();
+                return true;
+            }
+            for (int i = 0; i < (index - 1%length); i++) {
+                current = current->getNext();
+            }
+            dataTail->setNext(current->getNext());
+            current->setNext(dataHead);
+            length+=data.size();
+            if (index%length == length-1) {
+                tail = dataTail;
+            }
             return true;
         }
 
@@ -101,6 +144,14 @@ template<typename T> class CircularlyLinkedList {
                 current = current->getNext();
             }
             return current->getData();
+        }
+
+        Node<T>* getNode(const int index) {
+            auto current = head;
+            for (int i = 0; i < index % length; i++) {
+                current = current->getNext();
+            }
+            return current;
         }
 
         void printList() {
